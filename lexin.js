1609201ed3e793c4b80c593418a894c4052eab45
Jone2008 <70353049+Jone2008@users.noreply.github.com>
2020-10-19 17:37:23 +0800

    Update lexin.js
    感谢并修改自iepngs乐心健康脚本，仅复制用于修改基础步数，仅限自用。

--- lexin.js
+++ lexin.js
@@ -13,7 +13,7 @@
 // ^https?:\/\/sports\.lifesense\.com\/sport_service\/sport\/sport\/uploadMobileStepV2 url script-request-body https://raw.githubusercontent.com/iepngs/Script/master/lxhealth/index.js
 
 // [local_tasK]
-// 16 9 * * * https://raw.githubusercontent.com/iepngs/Script/master/lxhealth/index.js, tag=lxhealth, enabled=true
+// 58 19 * * * https://raw.githubusercontent.com/Jone2008/QuantumultX/master/lexin.js, tag=乐心运动, enabled=true
 
 // ----------------------------------------------------------------------------
 
