03e666ef72504a592a664f998675ec7f0e8af8cf
Jone2008 <70353049+Jone2008@users.noreply.github.com>
2024-11-25 10:33:35 +0800

    Create Mtxx.js

---
+++ Mtxx.js
@@ -0,0 +1,39 @@
+/*
+拷贝自：https://github.com/githubdulong/Script/blob/master/Mtxx.js
+自行修改了一下QX的mitm导入，直接引用重写即可
+
+【Quantumult_X】
+
+[rewrite_local]
+^https?://(api|h5).xiuxiu.meitu.com/(?!(v\d/feed/|v\d/search/|v\d/channel/)) url script-response-body https://raw.githubusercontent.com/githubdulong/Script/master/Mtxx.js
+
+[MITM]
+hostname = api.xiuxiu.meitu.com, h5.xiuxiu.meitu.com
+
+************************************************
+
+【Surge】
+[Script]
+美图秀秀 = type=http-response,requires-body=1,max-size=0,pattern=^https?://(api|h5).xiuxiu.meitu.com/(?!(v\d/feed/|v\d/search/|v\d/channel/)),script-path=https://raw.githubusercontent.com/githubdulong/Script/master/Mtxx.js
+
+[MITM]
+hostname: api.xiuxiu.meitu.com, h5.xiuxiu.meitu.com
+
+*/
+
+var obj = JSON.parse($response.body);
+
+if (obj && obj.data) {
+    obj.data.vip_type = 1;
+    obj.data.expire_days = -9999999999;
+    obj.data.is_expire = 0;
+    obj.data.in_valid_time = 5576488923;
+    obj.data.is_valid_user = 1;
+    obj.data.valid_time = 5576488923;
+    obj.data.home_prompt = "粉钻会员 2100年1月1日到期";
+    obj.data.home_btn_prompt = "已解锁";
+} else {
+    console.log("obj or obj.data is undefined");
+}
+
+$done({ body: JSON.stringify(obj) });
